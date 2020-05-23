285d515f51e2d6fef893e4c706d2e2d7b98c8631
Anthony Nata <nataanthoni@gmail.com>
2020-05-23 13:33:28 -0700

    Finish Bottom sheets

--- app/src/main/java/com/kweracodes/ezydialog/MainActivity.kt
+++ app/src/main/java/com/kweracodes/ezydialog/MainActivity.kt
@@ -7,10 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.bottom_sheet.*
+import kotlinx.android.synthetic.main.signin_bottom_sheet.*
+import kotlinx.android.synthetic.main.signup_bottom_sheet.*
 
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,36 @@ class MainActivity : AppCompatActivity() {
             //calling this method to show our android custom alert dialog
             openDialogOne()
         }
+
+        //Handles bottom sheet
+        var bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet)
+        openDialog2.setOnClickListener {
+            if (bottomSheetBehavior.state != BottomSheetBehavior.STATE_EXPANDED) {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            } else {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            }
+        }
+
+        val bottomSheetBehavior2 = BottomSheetBehavior.from(signin_bottom_sheet)
+        openDialog3.setOnClickListener {
+            if (bottomSheetBehavior2.state != BottomSheetBehavior.STATE_EXPANDED) {
+                bottomSheetBehavior2.state = BottomSheetBehavior.STATE_EXPANDED
+            } else {
+                bottomSheetBehavior2.state = BottomSheetBehavior.STATE_COLLAPSED
+            }
+        }
+
+        val bottomSheetBehavior3 = BottomSheetBehavior.from(signup_bottom_sheet)
+        openDialog4.setOnClickListener {
+            if (bottomSheetBehavior3.state != BottomSheetBehavior.STATE_EXPANDED) {
+                bottomSheetBehavior3.state = BottomSheetBehavior.STATE_EXPANDED
+            } else
+
+                bottomSheetBehavior3.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        }
+
     }
 
     private fun openDialogOne() {
@@ -41,5 +76,6 @@ class MainActivity : AppCompatActivity() {
         val alertDialog: AlertDialog = builder.create()
         alertDialog.show()
     }
+
 
 }
